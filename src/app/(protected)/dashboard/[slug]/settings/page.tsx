f48d12c1072e92ebcd240cda9a3529a3cce62d97
Wondr <wondr.design@gmail.com>
2025-08-04 01:35:55 +0100

    v1.3.3

--- src/app/(protected)/dashboard/[slug]/settings/page.tsx
+++ src/app/(protected)/dashboard/[slug]/settings/page.tsx
@@ -1,9 +1,10 @@
+import Billing from "@/components/global/billing";
 import React from "react";
 
 type Props = {};
 
 const Page = (props: Props) => {
-  return <div>page</div>;
+  return <Billing />;
 };
 
 export default Page;
